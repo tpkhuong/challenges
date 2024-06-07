07580c07f96e74709dc195a192940cfa6a371239
Toan <tpkhuong@gmail.com>
2024-06-07 04:04:50 -0400

    enjoyed the effort/process working on importing components to Feature. Photosnap multi page website.

--- advanced/nextJsProjects/photosnapMultiPageWebsite/src/app/features/page.js
+++ advanced/nextJsProjects/photosnapMultiPageWebsite/src/app/features/page.js
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "./Features.module.css";
+import LogoNavBar from "../components/server/LogoNavbar/index.js";
+import MobileNavMenu from "../components/server/MobileNavMenu/index.js";
+import FooterBanner from "../components/server/FooterBanner/index.js";
+import Footer from "../components/server/Footer/index.js";
 
 export default function FeaturesPage({ children }) {
   return (
