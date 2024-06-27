f257cfe1b27c040b1ceafd8f54dde64f509f8826
Toan <tpkhuong@gmail.com>
2024-06-27 04:03:02 -0400

    enjoyed the effort/process working on menu btn animation when user click on menu btn for mobile to close menu bottom line.

--- advanced/nextJsProjects/photosnapMultiPageWebsite/src/app/testing/page.js
+++ advanced/nextJsProjects/photosnapMultiPageWebsite/src/app/testing/page.js
@@ -5,7 +5,7 @@ export default function TestingPage({ children }) {
   return (
     <React.Fragment>
       <h1 className={styles[`title`]}>This is Next js.</h1>
-      <button data-mobilebtnclick="false" className={styles[`menu-btn`]}>
+      <button data-mobilebtnclick="" className={styles[`menu-btn`]}>
         <span className={`${styles[`top`]} ${styles[`line`]}`}></span>
         <span className={`${styles[`middle`]} ${styles[`line`]}`}></span>
         <span className={`${styles[`bottom`]} ${styles[`line`]}`}></span>
