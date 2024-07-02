ed339a847343d9ddbbbef1615468a3fbad312bcc
Toan <tpkhuong@gmail.com>
2024-07-02 04:01:52 -0400

    enjoyed the effort/process working designo website initial files.

---
+++ guru/nextProject/designoMultiPageWebsite/src/layout.js
@@ -0,0 +1,26 @@
+import "../styles/globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="author" content="KhuongCorp" />
+        <meta name="description" content="Building Next js" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="shortcut icon"
+          href="/favicon-32x32.png"
+          type="image/x-icon"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ guru/nextProject/designoMultiPageWebsite/src/page.js
@@ -0,0 +1,5 @@
+import styles from "../styles/IndexPage.module.css";
+
+export default function RootPage({ children }) {
+  return <React.Fragment></React.Fragment>;
+}
