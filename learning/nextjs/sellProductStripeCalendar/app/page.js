ca66b89e35e601e09a6eb97378a62c24ee89bfa4
Toan <tpkhuong@gmail.com>
2024-09-28 07:06:31 -0400

    enjoyed the effort/process of building algorithm nextjs and wix headless. Added LoginBar component to index page.

--- learning/nextjs/sellProductStripeCalendar/app/page.js
+++ learning/nextjs/sellProductStripeCalendar/app/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Calendar } from "@nextui-org/calendar";
 import styles from "../styles/Home.module.css";
+import LoginBar from "./components/client/LoginBar/index.js";
 
 export default function Home({ children }) {
   return (
     <React.Fragment>
       <section>
-        <Calendar />
+        {/* <Calendar /> */}
+        <LoginBar />
       </section>
     </React.Fragment>
   );
