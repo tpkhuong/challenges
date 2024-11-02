869fe37e49e878196f21d59068bba25484d2d0a1
Toan <tpkhuong@gmail.com>
2024-11-02 07:02:52 -0400

    enjoyed the effort/process of building algorithm stripe with nextjs. Will get postgre, prisma, and nextjs work.

--- learning/nextjs/productStripe/app/page.js
+++ learning/nextjs/productStripe/app/page.js
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import { PrismaClient } from "@prisma/client";
 import styles from "../styles/HomePage.module.css";
@@ -7,17 +5,25 @@ import styles from "../styles/HomePage.module.css";
 const prisma = new PrismaClient();
 
 export default function RootPage({ children }) {
+  const testing = testPrisma();
   return (
     <React.Fragment>
       <a href="#main-content" className="skip-link">
         Skip to Main Content
       </a>
       <h1 className={styles[`title`]}>This is the Root Page.</h1>
-      <button onClick={testPrisma}>click me</button>
     </React.Fragment>
   );
 }
 
-function testPrisma(event) {
+async function testPrisma(event) {
   console.log(event, "event");
+  const newUser = await prisma.user.create({
+    data: {
+      name: "Deadpool",
+      email: "deadpool@marvel.net",
+    },
+  });
+
+  console.log(newUser, "newUser");
 }
