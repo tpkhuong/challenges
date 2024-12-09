ab460b4a6f591b07d363008b94a0f74c6d8fb02b
Toan <tpkhuong@gmail.com>
2024-12-09 04:01:38 -0500

    enjoyed the effort/process of building algorithm for supabase and stripe. Inner Join query works.

--- learning/nextjs/supabaseProductStripe/app/api/products/route.js
+++ learning/nextjs/supabaseProductStripe/app/api/products/route.js
@@ -2,10 +2,27 @@ import { NextRequest, NextResponse } from "next/server";
 import { supabase } from "../../../utils/supabase/client";
 
 export async function GET(req, res) {
-  const { data, error } = await supabase.from("products").select("*");
+  // const { data, error } = await supabase.from("products").select("*");
+
+  /**
+   * JOIN
+   * **/
+
+  const { data, error } = await supabase.from("order_item").select(
+    `
+    *,
+    order_details(id)
+    `
+  );
+
+  console.log(data, "inner join");
 
   return NextResponse.json({
     message: "This is GET from api products",
-    productsData: data,
+    productsDataInnerJoin: data,
   });
+  // return NextResponse.json({
+  //   message: "This is GET from api products",
+  //   productsData: data,
+  // });
 }
