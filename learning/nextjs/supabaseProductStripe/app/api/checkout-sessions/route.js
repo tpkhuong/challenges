e93be8333a4941ca95ae764c3af8e6b4870abdd1
Toan <tpkhuong@gmail.com>
2025-01-13 04:02:07 -0500

    enjoyed the effort/process of building algorithm for supabase and stripe. Entered algorithm for creating a price obj.

--- learning/nextjs/supabaseProductStripe/app/api/checkout-sessions/route.js
+++ learning/nextjs/supabaseProductStripe/app/api/checkout-sessions/route.js
@@ -17,23 +17,40 @@ export async function POST(request, response) {
   // const body = await request.json();
   // const priceId = body;
 
-  const session = await stripe.checkout.sessions.create({
-    ui_mode: "embedded",
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price: `price_1234`,
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    return_url: `${request.headers.origin}/return?session_id={CHECKOUT_SESSION_ID}`,
-    // return_url: `${request.headers.get(
-    //   "origin"
-    // )}/return?session_id={CHECKOUT_SESSION_ID}`,
+  /**
+   * create price object
+   * **/
+
+  const price = await stripe.prices.create({
+    currency: "usd",
+    unit_amount: 300,
+    // recurring: {
+    //   interval: 'month',
+    // },
+    product_data: {
+      name: "Premium Hair",
+    },
   });
 
-  return NextResponse.send({ clientSecret: session.client_secret });
+  console.log(price, "price");
+
+  // const session = await stripe.checkout.sessions.create({
+  //   ui_mode: "embedded",
+  //   payment_method_types: ["card"],
+  //   line_items: [
+  //     {
+  //       price: `price_1234`,
+  //       quantity: 1,
+  //     },
+  //   ],
+  //   mode: "payment",
+  //   return_url: `${request.headers.origin}/return?session_id={CHECKOUT_SESSION_ID}`,
+  //   // return_url: `${request.headers.get(
+  //   //   "origin"
+  //   // )}/return?session_id={CHECKOUT_SESSION_ID}`,
+  // });
+
+  // return NextResponse.send({ clientSecret: session.client_secret });
 
   // return NextResponse.json({
   //   id: session.id,
