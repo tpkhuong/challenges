5f63360b97c8dcbc6de90930f078fe798a9c636e
Toan <tpkhuong@gmail.com>
2025-06-28 03:42:59 -0400

    enjoyed the effort/process of creating starting files for contact page.

---
+++ guru/nextProject/designoMultiPageWebsite/src/app/contact/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./ContactPage.module.css";
+
+export default function ContactPage({ children }) {
+  return (
+    <React.Fragment>
+      <a href="#main-content" className="skip-link">
+        Skip to Main Content
+      </a>
+    </React.Fragment>
+  );
+}
