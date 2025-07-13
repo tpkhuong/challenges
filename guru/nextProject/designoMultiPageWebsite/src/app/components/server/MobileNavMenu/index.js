402c5d52e7f71d35c9eac8d4635b419471ae562b
Toan <tpkhuong@gmail.com>
2025-07-13 03:57:46 -0400

    enjoyed the effort/process of adding circle img to mobile,tablet and desktop layout. Keyboard tabbing for mobile menu button and when user is tabbing through the mobile nav menu. Completed.

--- guru/nextProject/designoMultiPageWebsite/src/app/components/server/MobileNavMenu/index.js
+++ guru/nextProject/designoMultiPageWebsite/src/app/components/server/MobileNavMenu/index.js
@@ -54,5 +54,90 @@ export default function MobileNavMenu({ children, props }) {
 }
 
 function TabbingToCorrectElement(event) {
-  console.log(event, "event");
+  // get link element
+  const linkButton = event.target;
+  // if linkButton is truthy run algorithm
+  // get element id attribute
+  const elementAttribute = linkButton.getAttribute("id");
+  if (typeof elementAttribute == "string" && elementAttribute) {
+    // destructure event obj
+    const { shiftKey, code } = event;
+    // if shift is not pressed && focus is on contact link mobile menu button
+    if (elementAttribute == "contact-selector" && !shiftKey && code == "Tab") {
+      event.preventDefault();
+      // focus on animated mobile nav menu button
+      document.getElementById("mobile-btn-selector").focus();
+      return;
+    }
+    // if shift is pressed && focus is on our company link mobile menu button
+    if (elementAttribute == "company-selector" && shiftKey && code == "Tab") {
+      event.preventDefault();
+      // focus on animated mobile nav menu button
+      document.getElementById("mobile-btn-selector").focus();
+    }
+  }
 }
+
+function testingUnderstanding(firstName, lastName) {
+  console.log(this, "what is 'this'");
+  console.log(firstName, "firstName");
+  console.log(lastName, "lastName");
+
+  const makeSentence = `what is your name? My first name is ${firstName} and my last name is ${lastName}. My full name is ${firstName} ${lastName}.`;
+
+  return makeSentence;
+}
+
+// .call
+testingUnderstanding.call(
+  { firstName: "Marvel", lastName: "Lotto", date: "7-12-2025" },
+  "kickAssDude",
+  "coolDude"
+);
+
+// .apply
+testingUnderstanding.apply(
+  { firstName: "Marvel", lastName: "Lotto", date: "7-12-2025" },
+  [
+    "cool",
+    "awesome",
+    {
+      done: true,
+      lastName: "Lakers",
+      sayHi: (greet) => {
+        console.log(`I want to say ${greet}`);
+      },
+    },
+    "street name",
+  ]
+);
+// .apply
+testingUnderstanding.apply(
+  { firstName: "Marvel", lastName: "Lotto", date: "7-12-2025" },
+  ["cool guy", "cool dude"]
+);
+
+// .bind
+const finalBindFunc = testingUnderstanding.bind(
+  { firstName: "Marvel", lastName: "Lotto", date: "7-12-2025" },
+  "Cool person"
+);
+
+("use strict");
+
+function testForBind(...args) {
+  console.log(this, "this");
+  console.log(...args, "args");
+}
+// .bind
+const firstBindFunc = testForBind.bind(
+  { firstName: "Marvel", lastName: "Lotto", date: "7-12-2025" },
+  "Cool person"
+);
+
+const secondBindFunc = firstBindFunc.bind(
+  { firstName: "Marvel", lastName: "Lotto", date: "7-12-2025" },
+  "cooler person"
+);
+
+secondBindFunc("most cool person"); //Cool person cooler person most cool person
