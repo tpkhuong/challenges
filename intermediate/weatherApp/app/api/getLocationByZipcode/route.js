d354a9d5f7553656feb5eb59aa327f20cfcb1b3e
Toan <tpkhuong@gmail.com>
2026-05-17 04:01:44 -0400

    enjoyed the effort/process algorithm to render recent searches to UI. Making API call to api ninja within app using auth key in api route file.

--- intermediate/weatherApp/app/api/getLocationByZipcode/route.js
+++ intermediate/weatherApp/app/api/getLocationByZipcode/route.js
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req, res) {
+  console.log(req.headers, "req");
   const response = await fetch(
     "https://api.api-ninjas.com/v1/geocoding?zipcode=30308",
     {
       headers: {
-        Authorization: `Bearer ${hep6buoD1Ddy4SbObyaI2BenJxFQY6l5gROxLdOw}`,
         "x-api-key": "hep6buoD1Ddy4SbObyaI2BenJxFQY6l5gROxLdOw",
         "Content-Type": "application/json",
       },
